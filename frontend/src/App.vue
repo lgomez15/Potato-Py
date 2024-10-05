584af14a5413d36b3d9f6115f8a0501963b8e280
curtocode <alejandrogomezcurto@usal.es>
2024-10-05 13:33:20 +0200

    variable necesaria a weather

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,11 +2,13 @@
   import { RouterView } from 'vue-router';
   import InformationPanel from './components/InformationPanel.vue';
   import HumidityMonitor from './components/HumidityMonitor.vue';
+import WeatherVar from './components/weatherVar.vue';
 
 </script>
 
 <template>
   <div>
+    <WeatherVar city="Madrid"/>
     <InformationPanel/>
     <HumidityMonitor/>
     <RouterView/>
