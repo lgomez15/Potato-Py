3910539a87c68f065e794864bf067b11507e6434
Luis Roberto Gómez Veiga <gomez@usal.es>
2024-10-05 13:05:54 +0200

    humidity_Sensor ready

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
   import { RouterView } from 'vue-router';
   import InformationPanel from './components/InformationPanel.vue';
+  import HumidityMonitor from './components/HumidityMonitor.vue';
 
 </script>
 
 <template>
   <div>
     <InformationPanel/>
+    <HumidityMonitor/>
     <RouterView/>
   </div>
 </template>
