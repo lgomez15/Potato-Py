21a5de763ffd5ea535d25b37dfe5f756bc2a13a7
Luis Roberto Gómez Veiga <gomez@usal.es>
2024-10-05 10:53:27 +0200

    v1.1

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import AboutView from "../views/AboutView.vue";
-import CarView from "../views/CarView.vue";
-import ContactView from "../views/ContactView.vue";
-import NotFoundView from "../views/404view.vue";
+
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
@@ -16,28 +13,6 @@ const router = createRouter({
             path: "/home",
             redirect: "/"
         },
-        {
-            path: "/about",
-            name: "about",
-            component: AboutView
-        },
-        {
-            path: "/car/:id",
-            name: "car",
-            component: CarView,
-            children: [
-                {
-                    path: "contact",
-                    component: ContactView
-
-                }
-            ]
-        },
-        {
-            path: "/:pathMatch(.*)*",
-            name: "not-found",
-            component: NotFoundView
-        }
     ]
 });
 
--- frontend/src/views/AboutView.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div>
-        <h1>About</h1>
-        <p>Welcome to your about page with Vue.js app!</p>
-    </div>
-</template>
